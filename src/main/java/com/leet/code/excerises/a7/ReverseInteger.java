b74faba6bab206077bbc0e3ee83f7863eb817552
Adedamola <adeleke.adedamola@gmail.com>
2019-10-14 13:49:46 -0600

    Creating package

---
+++ src/main/java/com/leet/code/excerises/a7/ReverseInteger.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leet.code.excerises.a7;
+
+/**
+ *
+ * @author Ade
+ *
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ *
+ * Example 1:
+ *
+ * Input: 123 Output: 321
+ *
+ * Example 2:
+ *
+ * Input: -123 Output: -321
+ *
+ * Example 3:
+ *
+ * Input: 120 Output: 21
+ *
+ * Note: Assume we are dealing with an environment which could only store
+ * integers within the 32-bit signed integer range: [−231, 231 − 1]. For the
+ * purpose of this problem, assume that your function returns 0 when the
+ * reversed integer overflows.
+ */
+public class ReverseInteger {
+
+    public int reverse( int x ) {
+        int reversedX = 0;
+        
+        
+        return reversedX;
+    }
+}
